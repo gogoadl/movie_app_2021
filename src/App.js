3ce871115375a6d7f4b4ac4c4cfddbe76dae1242
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2021-03-21 22:39:40 +0900

    [ADD] JSX & props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+function Data({name}) {
+  return <h1>I like {name} </h1>
+}
+
+
+const datas = [
+  { name : "황지원" },
+  { name : "바보" },
+  { name : "사랑해" }
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {datas.map(names => <Data name ={names.name}></Data>)}
     </div>
   );
 }
