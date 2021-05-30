afc08f14f8e8caf67324257daea90123715865e0
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2021-05-30 18:36:35 +0900

    Movie 컴포넌트 추가, 비동기 요청, CSS 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie"
+import "./App.css";
 
+// function components has no state
+class App extends React.Component {
 
-const datas = [
-  { id : 1, name : "황지원" },
-  { id : 2, name : "바보" },
-  { id : 3, name : "사랑해" }
-]
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-function Data({name}) {
-  return (
-  <div>
-  <h1>I like {name} </h1>
-  </div>
-  )
-}
+  getMovies = async () => { // 화살표 함수, async 함수로 선언하여 비동기 데이터 받아옴
+    const { // es6 style data.data.movies 대신에 movies로 바로 접근가능
+      data: { // await 키워드 추가하여 비동기 요청이 끝날때까지 기다린 후 movies에 데이터 채워줌
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating") 
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
 
-function App() {
-  return (
-    <div className="App">
-    {datas.map(names => <Data key={names.id} name ={names.name}></Data>)}
-    </div>
-  );
+  render(){
+    console.log("render");
+    const { isLoading, movies } = this.state;
+    return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader_text">Loading..</span>
+         </div>
+        ) : ( 
+          <div className="movies">
+            {
+              movies.map(movie => {
+                return (<Movie 
+                  key = {movie.id}
+                  id = {movie.id} 
+                  year = {movie.year} 
+                  title = {movie.title} 
+                  summary = {movie.summary} 
+                  poster = {movie.medium_cover_image}
+                />
+                );
+                })
+            }
+          </div>
+        )}
+      </section>
+    
+    );
+  }
 }
 
 export default App;
